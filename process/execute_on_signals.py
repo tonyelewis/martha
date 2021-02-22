74980ca380119a8b8ae835786d561308c8bfabc7
Tony Lewis <tonyelewis@users.noreply.github.com>
2021-02-22 07:45:04 +0000

    Sort some issues with execute_on_signals & tests

--- process/execute_on_signals.py
+++ process/execute_on_signals.py
@@ -6,16 +6,16 @@ from typing import Any, Callable, Dict, List
 
 @contextlib.contextmanager
 def execute_on_signals(action: Callable,
-					   *,
-					   signals: List[signal.Signals] = [ signal.SIGTERM, signal.SIGINT ],
-					   also_on_exit: bool = False,
-					   ):
+                       *,
+                       signals: List[signal.Signals] = [ signal.SIGTERM, signal.SIGINT ],
+                       also_on_normal_exit: bool = False,
+                       ):
 	'''
 	A context-manager that ensure the specified action is performed if/when any of the specified signals
 	are being handled. This doesn't block the signal (it re-raises it).
 
-	:param handle_signals : (optional; default [ signal.SIGTERM, signal.SIGINT ]) The signals to handle
-	:param also_on_exit   : Whether to also perform the action on normal exit
+	:param handle_signals      : (optional; default [ signal.SIGTERM, signal.SIGINT ]) The signals to handle
+	:param also_on_normal_exit : Whether to also perform the action on normal exit
 	'''
 	# Create a store of the handlers that were previously in place for each of the signals
 	prev_handler_of_signal: Dict[int, Any] = { sig: None for sig in signals }
@@ -40,5 +40,5 @@ def execute_on_signals(action: Callable,
 	yield
 
 	# If the action is to be performed on normal exit, perform it
-	if also_on_exit:
+	if also_on_normal_exit:
 		action()
--- test/process/test_execute_on_signals.py
+++ test/process/test_execute_on_signals.py
@@ -11,97 +11,98 @@ from typing import Any, Dict, List, Optional, Union
 
 from process.execute_on_signals import execute_on_signals
 
-LONG_TIME = datetime.timedelta( seconds=2 )
-SHORT_TIME = datetime.timedelta( seconds=0.002 )
+READY_TO_RECEIVE_SIGNAL_MSG = 'ready to receive signal'
+SOME_TIME = datetime.timedelta( seconds=1 )
+ZERO_TIME = datetime.timedelta( seconds=0 )
 
-def other_process_function( *,
-                            sleep_durn: datetime.timedelta,
-                            handle_signals: Optional[Union[bool,List[signal.Signals]]] = None,
-                            also_on_exit: bool,
-                            sendback_data: Optional[str],
-                            conn,
-                            ):
+def sleep_but_send_message_on_signals( *,
+                                       sleep_durn: datetime.timedelta,
+                                       signals_to_handle: Optional[List[signal.Signals]] = None,
+                                       also_on_normal_exit: bool,
+                                       sendback_mesg: Optional[str],
+                                       conn,
+                                       ):
 	'''
-	A function to run in another process and sleep for some time and optionally use a configured
-    execute_on_signals() context manager to send some specified data on a specified connection.
+	Set-up an execute_on_signals() context manager, with an action to send the specified message on
+	the specified connection, and then sleep for some time whilst that context manager is active
 
-	This can be used for testing the results of sending various signals, with and without execute_on_signals()
+	This is intended to be run in another process, for testing execute_on_signals()
 
-	:param sleep_durn     : The amount of time to sleep for
-	:param handle_signals : The signals to handle with execute_on_signals(), or None for default, or False to not use execute_on_signals()
-	:param also_on_exit   : Whether the execute_on_signals() (if being used) should also perform the action on exit
-	:param sendback_data  : The data that execute_on_signals() should send, if appropriate
-	:param conn           : The connection that execute_on_signals() should send the data through, if appropriate
+	:param sleep_durn          : The amount of time to sleep for
+	:param signals_to_handle   : The signals to handle with execute_on_signals(), or None for default
+	:param also_on_normal_exit : Whether the execute_on_signals() (if being used) should also perform the action on exit
+	:param sendback_mesg       : The data that execute_on_signals() should send, if appropriate
+	:param conn                : The connection that execute_on_signals() should send the data through, if appropriate
 	'''
 	# Prevent filling the output with stderr guff if/when this stops due to a signal
 	sys.stderr = io.StringIO()
 
-	# If not handling signals, just sleep and return (without execute_on_signals())
-	if handle_signals is not None and handle_signals == False:
-		time.sleep( sleep_durn / datetime.timedelta( seconds=1 ) )
-		return
-	
-	# Use a execute_on_signals() context manager
-	# (specify signals as handle_signals if that isn't None)
-	# and then sleep
-	kwargs: Dict[str, Any] = { 'also_on_exit' : also_on_exit }
-	if handle_signals:
-		kwargs['signals'] = handle_signals
-	with execute_on_signals( lambda:conn.send( sendback_data ), **kwargs ):
+	# Use an execute_on_signals() context manager (specifying 'signals' as signals_to_handle if that isn't None),
+	# send a message to indicate readiness, and then sleep
+	kwargs: Dict[str, Any] = { 'also_on_normal_exit' : also_on_normal_exit }
+	if signals_to_handle is not None:
+		kwargs['signals'] = signals_to_handle
+	with execute_on_signals( lambda:conn.send( sendback_mesg ), **kwargs ):
+		conn.send( READY_TO_RECEIVE_SIGNAL_MSG )
 		time.sleep( sleep_durn / datetime.timedelta( seconds=1 ) )
 
 
-def other_process_executes_action( also_on_exit: bool,
-                                   test_signal: Optional[int] = None,
-                                   handle_signals: Optional[Union[bool,List[signal.Signals]]] = None,
+def other_process_executes_action( also_on_normal_exit: bool,
+                                   signal_to_send: Optional[int] = None,
+                                   signals_to_handle: Optional[List[signal.Signals]] = None,
                                    ) -> bool:
 	'''
-	Whether a separate process using execute_on_signals() (as specified) performs the action
+	Whether a separate process using execute_on_signals() (configured as specified) performs the action
 	if sent the specified signal
 
-	:param also_on_exit   : Whether the execute_on_signals() should be instructed to also perform the action on exit
-	:param test_signal    : [Optional] The signal to send (or default to None, meaning sending none)
-	:param handle_signals : The signals the other process should handle with execute_on_signals(), or None for default, or False to not use execute_on_signals()
+	:param also_on_normal_exit : Whether the execute_on_signals() should be instructed to also perform the action on exit
+	:param signal_to_send      : [Optional] The signal to send (or default to None, meaning sending none)
+	:param signals_to_handle   : The signals the other process should handle with execute_on_signals(), or None for default
 	'''
+	EXECUTED_ACTION_MSG = 'executed action'
 
-	# Prepare some data, a multiprocessing pipe, and kwargs for other_process_function()
-	DATA = 'this is the data'
+	# Prepare a multiprocessing pipe, and kwargs for sleep_but_send_message_on_signals()
 	conn_a, conn_b = multiprocessing.Pipe()
 	process_kwargs = {
-		'also_on_exit'  : also_on_exit,
-		'conn'          : conn_b,
-		'sendback_data' : DATA,
-		'sleep_durn'    : SHORT_TIME if test_signal is None else LONG_TIME,
+		'also_on_normal_exit' : also_on_normal_exit,
+		'conn'                : conn_b,
+		'sendback_mesg'       : EXECUTED_ACTION_MSG,
+		'sleep_durn'          : ZERO_TIME if signal_to_send is None else SOME_TIME,
 	}
-	if handle_signals is not None:
-		process_kwargs['handle_signals'] = handle_signals
+	if signals_to_handle is not None:
+		process_kwargs['signals_to_handle'] = signals_to_handle
 
-	# Create a process to run other_process_function and start it
+	# Create a process to run sleep_but_send_message_on_signals, start it, and wait for it to indicate that it's ready
+	# (to receive any signal)
 	process_handle = multiprocessing.Process(
-		target = other_process_function,
+		target = sleep_but_send_message_on_signals,
 		kwargs = process_kwargs,
 	)
 	process_handle.start()
+	assert conn_a.recv() == READY_TO_RECEIVE_SIGNAL_MSG
 
-	# If a signal is to be sent, give the process a short time to start up and then send the signal to it
-	if test_signal is not None:
-		time.sleep(SHORT_TIME / datetime.timedelta( seconds=1 ) )
+	# If a signal is to be sent, do so
+	if signal_to_send is not None:
 		assert process_handle.pid is not None
-		os.kill(process_handle.pid, test_signal)
+		os.kill(process_handle.pid, signal_to_send)
 
 	# Wait for the process to complete and determine whether it sent the requested data back
 	process_handle.join()
 	received = conn_a.recv() if conn_a.poll() else None
-	assert received is None or received == DATA
-	return received == DATA
+	assert received is None or received == EXECUTED_ACTION_MSG
+	return received == EXECUTED_ACTION_MSG
 
 
-@pytest.mark.parametrize('also_on_exit', (True, False))
-@pytest.mark.parametrize('test_signal', (signal.SIGTERM, signal.SIGINT, None))
-@pytest.mark.parametrize('handle_signals', (False, None))
-def test_execute_on_signals_situations(also_on_exit, test_signal, handle_signals):
+@pytest.mark.parametrize('also_on_normal_exit', (True, False))
+@pytest.mark.parametrize('signal_to_send', (signal.SIGTERM, signal.SIGINT, None))
+@pytest.mark.parametrize('signals_to_handle', ([], None))
+def test_execute_on_signals_situations(also_on_normal_exit, signal_to_send, signals_to_handle):
 	assert other_process_executes_action(
-		also_on_exit=also_on_exit,
-		test_signal=test_signal,
-		handle_signals=handle_signals
-	) == (handle_signals is None and (test_signal is not None or also_on_exit))
+		also_on_normal_exit=also_on_normal_exit,
+		signal_to_send=signal_to_send,
+		signals_to_handle=signals_to_handle
+	) == (
+		( signal_to_send and ( signals_to_handle is None or signal_to_send in signals_to_handle ) )
+		or
+		( not signal_to_send and also_on_normal_exit )
+	)
